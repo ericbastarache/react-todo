bd9680e565546f717b4df230cd93e97f1d7b715d
ericb <eric_bastarache@hotmail.com>
2017-03-15 02:37:28 -0400

    Created preliminary task component

---
+++ src/components/Task.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+import FontAwesome from 'react-fontawesome';
+
+class Task extends Component {
+  render () {
+    return (
+
+    );
+  }
+}
+
+export default Task;
