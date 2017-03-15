9156747dd8217f1ca50333b3b63744ae767638de
ericb <eric_bastarache@hotmail.com>
2017-03-15 02:58:47 -0400

    Added task component to test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Task from './components/Task';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
